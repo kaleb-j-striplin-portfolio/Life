1924ea0c189fdf4890a89e942e2ce5daaf2b580b
Kaleb Striplin <kaleb.striplin@thermofisher.com>
2026-01-23 15:07:08 -0800

    Adds .cargo with config.toml. Updates main with blinky implementation to check flashing works.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embedded_hal::{digital::OutputPin, delay::DelayNs};
+use microbit::{
+    board::Board,
+    hal::{
+        timer::Timer,
+    },
+};
+
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
 mod life;
 use life::*;
 
-fn main() {
-    println!("Hello, world!");
+enum State {
+    LedOn,
+    LedOff,
+}
+
+#[entry]
+fn init() -> ! {
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
+    board.display_pins.col1.set_low().unwrap();
+
+    let mut state = State::LedOff;
+
+    loop {
+        state = match state {
+            State::LedOff => {
+                board.display_pins.row1.set_high().unwrap();
+                rprintln!("high");
+                State::LedOn
+            }
+            State::LedOn => {
+                board.display_pins.row1.set_low().unwrap();
+                rprintln!("low");
+                State::LedOff
+            }
+        };
+        timer.delay_ms(2000);
+    }
 }
