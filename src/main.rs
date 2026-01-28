817ac752fa9fa1fe740b67eec28255b10dd0c4f4
Kaleb Striplin <kaleb.striplin@thermofisher.com>
2026-01-27 21:49:18 -0800

    Updates loop with a call to life to iterate the board. Upates loop with a call to done to start a new board.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,23 @@ enum State {
     LedOff,
 }
 
+fn generate_random_board(rng: &mut Rng) -> [[u8; 5]; 5] {
+    let mut random_game_board: [[u8; 5]; 5] = [[0u8; 5]; 5];
+    let mut buf: [u8; 25] = [0; 25];
+    rng.random(&mut buf);
+
+    rprintln!("buffer {:?}", buf);
+    let mut i = 0;
+    for y in 0..5 {
+        for x in 0..5 {
+            random_game_board[y][x] = buf[i]%2;
+            i+=1;
+        }
+    }
+    rprintln!("board {:?}", random_game_board);
+    random_game_board
+}
+
 #[entry]
 fn init() -> ! {
     rtt_init_print!();
@@ -44,40 +61,44 @@ fn init() -> ! {
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
         ];
-    let mut random_game_board: [[u8; 5]; 5] = [[0u8; 5]; 5];
     let mut rng: Rng = Rng::new(board.RNG);
+    
+    // The program starts with a random board.
+    let mut random_game_board: [[u8; 5]; 5] = generate_random_board(&mut rng);
     let mut display: Display = Display::new(board.display_pins);
     let mut state: State = State::LedOff;
 
     loop {
-        let mut buf: [u8; 25] = [0; 25];
-        rng.random(&mut buf);
+        
+        // let pressed = button.is_low().unwrap();
+        // state = match (pressed, state) {
+        //     (true, State::LedOn) => {
+        //         display.show(&mut timer, random_game_board, 10);
+        //         rprintln!("heart");
+        //         State::LedOff
+        //     }
+        //     _ => {
+        //         display.show(&mut timer, empty_board_heart, 10);
+        //         rprintln!("none");
+        //         State::LedOn
+        //     }
+        // };
 
-        rprintln!("buffer {:?}", buf);
 
-        let mut i = 0;
-        for y in 0..5 {
-            for x in 0..5 {
-                random_game_board[y][x] = buf[i]%2;
-                i+=1;
-            }
+        // Otherwise, normal Life steps are taken.
+        life(&mut random_game_board);
+        
+        // The program runs the game at 10 frames per second (updates once per 100ms).
+        display.show(&mut timer, random_game_board, 100);
+        
+        // Otherwise, if the program reaches a state where all cells on the board are off, 
+        if done(& random_game_board) {
+            // the program waits 5 frames (0.5s). 
+            // If it has not received a button press, it then starts with a new random board.
+            timer.delay_ms(500);
+            random_game_board = generate_random_board(&mut rng);
         }
 
-        rprintln!("board {:?}", random_game_board);
-
-        let pressed = button.is_low().unwrap();
-        state = match (pressed, state) {
-            (true, State::LedOn) => {
-                display.show(&mut timer, random_game_board, 10);
-                rprintln!("heart");
-                State::LedOff
-            }
-            _ => {
-                display.show(&mut timer, empty_board_heart, 10);
-                rprintln!("none");
-                State::LedOn
-            }
-        };
-        timer.delay_ms(100);
+        // timer.delay_ms(10);
     }
 }
